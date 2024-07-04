659f8d7c172b402b59884d9f585445e2b2bade99
Alekson Bruno <alek.pbruno@gmail.com>
2024-07-04 10:07:14 -0300

    update ex007.py

--- PythonExercícios/ex007.py
+++ PythonExercícios/ex007.py
@@ -1,4 +1,4 @@
 aluno = input('Nome do aluno: ')
-n1 = int(input('Digite a primeira nota: '))
-n2 = int(input('Digite a segunda nota: '))
-print(f'A média do aluno {aluno} foi {(n1+n2)/2}')
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print(f'A média do aluno {aluno} foi {(n1+n2)/2:.1f}')
