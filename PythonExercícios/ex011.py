ef7d1b646e77d5217024c4a232dc4a79328faf8d
Alekson Bruno <alek.pbruno@gmail.com>
2024-07-04 10:46:24 -0300

    Update ex011.py

--- PythonExercícios/ex011.py
+++ PythonExercícios/ex011.py
@@ -2,4 +2,4 @@ lar = float(input('Largura da parede: '))
 alt = float(input('Altura da parde: '))
 area = lar * alt
 tinta = area / 2
-print(f'Sua parede tem {area:.2f}m², para pintá-la você vai precisar de {tinta} litros de tinta!')
+print(f'Sua parede tem {area}m², para pintá-la você vai precisar de {tinta} litros de tinta!')
