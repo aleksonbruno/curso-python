a20db438a852a04f8095e766a9633dff1d588cbe
Alekson Bruno <alek.pbruno@gmail.com>
2024-07-08 08:58:18 -0300

    add ex018

---
+++ PythonExercícios/ex018.py
@@ -0,0 +1,8 @@
+from math import cos, sin, tan
+angulo = float(input('Digite o valor de um ângulo: '))
+seno = sin(angulo)
+cosseno = cos(angulo)
+tangente = tan(angulo)
+print(f'O seno do ângulo {angulo} é {seno}')
+print(f'O cosseno do ângulo {angulo} é {cosseno}')
+print(f'A tangente do ângulo {angulo} é {tangente}')
